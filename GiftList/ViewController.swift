05db2b431aad96fed4e93b0a6486a351e61bbd28
Paul Sohal <paulsohal@gmail.com>
2015-06-18 10:49:49 -0700

    Redo the project structure

--- GiftList/ViewController.swift
+++ GiftList/ViewController.swift
@@ -8,18 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.tableView.dataSource = self;
+        self.tableView.delegate = self;
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //TABLE View Functions
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return 4;
     }
-
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        var cell = UITableViewCell();
+        cell.textLabel!.text = "Birthday Party"
+        return cell;
+    }
+    
 
 }
 
