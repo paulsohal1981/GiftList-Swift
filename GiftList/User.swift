78f8980b3f99243822914b6cfeaba9dac145d534
Paul Sohal <paulsohal@gmail.com>
2015-11-02 19:04:08 -0800

    adding user object

---
+++ GiftList/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  Papoose
+//
+//  Created by paul sohal on 10/19/15.
+//  Copyright © 2015 Acceler Inc. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class User: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
